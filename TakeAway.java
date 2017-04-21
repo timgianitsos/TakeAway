8f4a7242d834754531725d5d9ce64669a72d9e9d
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2017-04-21 03:28:09 -0500

    AI

--- TakeAway.java
+++ TakeAway.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+//Inspired by a Cyberchase episode on PBS
 //TODO add output coloring
+
 public class TakeAway 
 {
 	final static Scanner scan = new Scanner(System.in);
-	final static int startPoints = 20;
-	final static int takeMin = 1;
-	final static int takeMax = 3;
+	final static int startPoints = 25;
+	final static int takeMin = 3; //TODO ensure min is less than max
+	final static int takeMax = 7;
 
 	public static void main(String[] args) { //TODO get game parameters from arguments
 		intro();
@@ -21,7 +23,7 @@ public class TakeAway
 		System.out.println("\nWelcome to Takeaway.");
 		System.out.println("You will play against the computer. The game starts with " + startPoints + " points and you will both ");
 		System.out.println("take turns subtracting between " + takeMin + " and " + takeMax + " points away from the total. ");
-		System.out.println("The player who is left with zero points at the beginning of their turn loses.");
+		System.out.println("The player who is left with too few points to take at the beginning of their turn loses.");
 	}
 
 	static void play() {
@@ -30,14 +32,16 @@ public class TakeAway
 		
 		int points = startPoints;
 		System.out.println("\nThe point total is " + points + ".");
-		while (points > 0) {
+		while (points >= takeMin) {
 			if (playerTurn) {
 				System.out.println("It's your turn player. How many points will you take away?");
 				points -= getInt();
 			}
 			else {
-				//TODO intelligent implement AI
-				int pointsTaken = (int)(Math.random() * (takeMax - takeMin + 1) + 1);
+				int position = points % (takeMin + takeMax);
+				double rand = Math.random();
+				int pointsTaken = position < takeMin ? (int)(rand * (takeMax - takeMin + 1) + takeMin):
+					(int)(rand * (Math.min(position, takeMax) - Math.max(position - takeMin + 1, takeMin) + 1)) + Math.max(position - takeMin + 1, takeMin);
 				System.out.println("Your opponent takes " + pointsTaken + " point(s).");
 				points -= pointsTaken;
 			}
