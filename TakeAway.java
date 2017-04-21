52018b940643757602794912c44db224e3f5cc00
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2017-04-21 00:00:11 -0500

    Initial commit

---
+++ TakeAway.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class TakeAway 
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Random rand = new Random();
+	}
+}
