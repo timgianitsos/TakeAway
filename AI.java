592a3f1bca8ca48bee0a29c2bcef734b32964ef6
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2017-04-22 18:28:58 -0500

    Improved behavior on poison number

--- AI.java
+++ AI.java
@@ -3,18 +3,20 @@ class AI {
 	static boolean easyMode = false;
 
 	public static int getMove(int currentPoints) {
+		int min = TakeAway.takeMin;
+		int max = TakeAway.takeMax;
 		if (easyMode) {
-			return (int)(Math.random() * (TakeAway.takeMax - TakeAway.takeMin + 1)) + TakeAway.takeMin;
+			return (int)(Math.random() * (max - min + 1)) + min;
 		}
 		else {
-			int position = currentPoints % (TakeAway.takeMin + TakeAway.takeMax);
+			int position = currentPoints % (min + max);
 			double rand = Math.random();
 			int pointsTaken;
-			if (position < TakeAway.takeMin) {
-				pointsTaken = (int)(rand * (TakeAway.takeMax - TakeAway.takeMin + 1) + TakeAway.takeMin); //Improve random behavior
+			if (position < min) {
+				pointsTaken = position <= min / 2 ? min: max;
 			}
 			else {
-				pointsTaken = (int)(rand * (Math.min(position, TakeAway.takeMax) - Math.max(position - TakeAway.takeMin + 1, TakeAway.takeMin) + 1)) + Math.max(position - TakeAway.takeMin + 1, TakeAway.takeMin);
+				pointsTaken = (int)(rand * (Math.min(position, max) - Math.max(position - min + 1, min) + 1)) + Math.max(position - min + 1, min);
 			}	
 			return pointsTaken;
 		}
